8cc05d5f9eaa64c7230e1797e1e51442adc15955
izelacerda <izelacerdajl@gmail.com>
2020-04-16 09:15:26 -0300

    update axios

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,12 @@ const environment_mode =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3333"
     : process.env.REACT_APP_API_URL;
+
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+
 const api = axios.create({
   baseURL: environment_mode
 });
+
 
 export default api;
--- src/views/pages/authentication/login/LoginJWT.js
+++ src/views/pages/authentication/login/LoginJWT.js
@@ -158,7 +158,7 @@ export default function LoginJWT() {
         </Form>
         <Container>
           <Content>
-          <Label>Mycare.med.br - Versão: 1.0.1 - 03/04/2020</Label>
+          <Label>Mycare.med.br - Versão: 1.0.1 - 16/04/2020</Label>
           </Content>
         </Container>
 
